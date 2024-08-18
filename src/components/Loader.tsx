e63180e17efab9bfc35b6b8f3005378a7935b48b
svkhrobbeck <svkhrobbeck@gmail.com>
2024-08-18 21:26:39 +0500

    feat: :sparkles: complete loader component

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,14 +1,24 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, View } from "react-native";
 import LottieView from "lottie-react-native";
 
 const Loader = () => {
-  return <LottieView style={styles.lottie} source={require("../assets/loader.json")} autoPlay loop />;
+  return (
+    <View style={styles.wrapper}>
+      <LottieView style={styles.lottie} source={require("../assets/loader.json")} autoPlay loop />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    backgroundColor: "#fdf6aa",
+    justifyContent: "center",
+    alignItems: "center",
+  },
   lottie: {
-    width: 350,
-    height: 350,
+    width: 250,
+    height: 250,
   },
 });
 
